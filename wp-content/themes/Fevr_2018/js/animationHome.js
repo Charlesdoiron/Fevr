c52da45fcde054e06d594c485ac24ef7abfcabe5
Charles d_Oiron <doironcharles@gmail.com>
2018-02-12 10:16:29 +0100

    🍁

--- wp-content/themes/Fevr_2018/js/animationHome.js
+++ wp-content/themes/Fevr_2018/js/animationHome.js
@@ -1,6 +1,6 @@
-const header = document.querySelector('.site-branding')
-const headerHeight = header.offsetHeight
-const menuItems = [...document.querySelectorAll('.main-navigation li a')]
-const menuBorder = [...document.querySelectorAll('.main-navigation .current-menu-item > a')]
-let logo = document.querySelector('.custom-logo-link img')
+// const header = document.querySelector('.site-branding')
+// const headerHeight = header.offsetHeight
+// const menuItems = [...document.querySelectorAll('.main-navigation li a')]
+// const menuBorder = [...document.querySelectorAll('.main-navigation .current-menu-item > a')]
+// let logo = document.querySelector('.custom-logo-link img')
 
--- wp-content/themes/Fevr_2018/js/stickyHeader.js
+++ wp-content/themes/Fevr_2018/js/stickyHeader.js
@@ -2,16 +2,20 @@
 // STIKY HEADER
 
 window.addEventListener('scroll', function() {
-let header = document.querySelector('.site-branding')
-let headerHeight = header.offsetHeight
-let menuItems = [...document.querySelectorAll('.main-navigation li a')]
-let menuBorder = [...document.querySelectorAll('.main-navigation .current-menu-item > a')]
-let logo = document.querySelector('.custom-logo-link img')
+const header = document.querySelector('.site-branding')
+const headerHeight = header.offsetHeight
+const menuItems = [...document.querySelectorAll('.main-navigation li a')]
+const menuBorder = [...document.querySelectorAll('.main-navigation .current-menu-item > a')]
+const logo = document.querySelector('.custom-logo-link img')
+const siteMain = document.querySelector('.site-main')
 
     if (!header.classList.contains('active') && (window.pageYOffset > headerHeight)) {
         header.classList.add('active')
         menuItems.map(item => item.classList.add('active'))
         menuBorder.map(item => item.classList.add('active'))
+        // Baisse le contenu du site
+        siteMain.style.paddingTop = headerHeight + "px";
+        console.log('dd')
         logo.src = "http://localhost:8888/wp-content/uploads/2017/09/logo.png"
     } else if (window.pageYOffset <= headerHeight && (header.classList.contains('active'))) {
         menuItems.map(item => item.classList.remove('active'))
